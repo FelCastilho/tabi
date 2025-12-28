982df02c9681ec186830ed7ff8ceaf66a57b2e42
Felipe Coutinho Castilho <castfelipe10@gmail.com>
2025-12-28 13:40:37 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// MENU MOBILE
+
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId);
+  const nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("active-menu-mobile");
+      toggle.classList.toggle("active-bx");
+    });
+  }
+};
+
+showMenu("bx", "menu-mobile");
+
+// SCROLL HEADER
+
+function scrollHeader() {
+  const nav = document.getElementById("header");
+
+  if (this.scrollY >= 50) nav.classList.add("active-header");
+  else nav.classList.remove("active-header");
+}
+
+window.addEventListener("scroll", scrollHeader);
+
+// GSAP ANIMATIONS
+
+gsap.registerPlugin(ScrollTrigger);
+
+
+
+
